bd807ae3e245800bef09b46d0e127b67af9e76dc
basalamahalam <basalamahalam@upi.edu>
2023-09-09 13:50:24 +0700

    database

--- source_code/connection.php
+++ source_code/connection.php
@@ -4,6 +4,6 @@
         static public $db_host = "localhost";
         static public $db_user = "root";
         static public $db_pass = "";
-        static public $db_name = "db_tp4dpbo";
+        static public $db_name = "db_partai";
     }   
 ?>
